e760bf4afab793c48606a1172fa8fad94360b17c
dhyan70 <dhyanpatel69@gmail.com>
2024-05-19 17:22:00 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import express from 'express'
 const app = express()
+
+
+import Userrouter from './Routes/user.routes.js'
+
+app.use("/api/user" , Userrouter )
 app.listen(3000 ,()=>{
-    console.log("server running ")
+    console.log("server running  ")
 })
