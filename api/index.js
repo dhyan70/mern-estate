c89dcac2215b16fa06060406eb8390791b279a48
dhyan70 <dhyanpatel69@gmail.com>
2024-05-19 17:38:51 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 import express from 'express'
 const app = express()
-
-
+import mongoose from 'mongoose'
 import Userrouter from './Routes/user.routes.js'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("successful")
+}).catch((e)=>{
+    console.log("error connecting to database")
+})
 
 app.use("/api/user" , Userrouter )
 app.listen(3000 ,()=>{
-    console.log("server running  ")
+    console.log("server running ")
 })
